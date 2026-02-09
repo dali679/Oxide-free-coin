9deaf6a3718af11d6fae81326d7f802b85a6e31f
dali679 <dalixman4@gmail.com>
2026-02-09 16:19:00 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    WEBHOOK_URL: "https://discord.com/api/webhooks/1470432997244407868/BVQBCS_XyFFkiT-LbR7SX-m79d4WCcd3V3RQAoYed2F9Km36B2GhrzKkjK1_g_qGOFFG",
+    REDIRECT_URL: "https://shop.playoxide.com/" 
+};
